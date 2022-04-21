b47886c581f10f71b9834595dfaaef22904a9373
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-04-21 19:22:37 +0200

    reestructuracion completa del esqueleto de la web, mas logico todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { FiltrosComponent } from './componentes/filtros/filtros.component';
+import { DatosComponent } from './componentes/datos/datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FiltrosComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/filtros/filtros.component.ts
+++ src/app/componentes/filtros/filtros.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltrosComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  ingrediente:string='';
+  activoB: boolean = true
+
+
+  buscar(){
+    
+  }
 }
